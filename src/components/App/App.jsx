7f29f713fab7978b4b1a1754aaa2093f9c5287b8
RhainUnity <schmidtjeremyr@gmail.com>
2025-12-20 19:48:01 -0800

    stage-1-frontend-12-20-25_3

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,1 +1,15 @@
 // src/components/App/App.jsx
+
+//  ///////////
+// import {useState, useEffect} from "react";
+// import { Routes, Route } from "react-router-dom";
+// import "./App.css";
+// import Header from "../Header/Header";
+// import RequireAuth from "../RequireAuth/RequireAuth.jsx";
+// import Main from "../Main/Main";
+// import Profile from "../Profile/Profile.jsx";
+// import Footer from "../Footer/Footer.jsx";
+
+function App() {}
+
+export default App;
